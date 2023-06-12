3340dad59ad9f228b378efe93d8662e8b38cb29f
fenitamas <124518729+fenitamas@users.noreply.github.com>
2023-06-12 17:45:46 +0300

    Add files via upload

---
+++ FoP-II/Kaku_Temesgen/function/Exercise_3.cpp
@@ -0,0 +1,30 @@
+//A c++ program to demonstrate a function with parameters and no return type.
+#include <iostream>
+using namespace std;
+
+// Function declaration
+void mult(float num1, float num2);
+
+int main() {
+
+  mult(2.7, 4.9);// Calling mult function with floating-point literals.
+  float number;
+  cout<<"Enter a floating-point number: ";
+  cin>>number;
+  mult(4.8,number); // calling mult function with floating-point literal and variable.
+
+  float num1, num2;
+  cout<<"Enter the first floating-point number: ";
+  cin>>num1;
+  cout<<"Enter the second floating-point number: ";
+  cin>>num2;
+  mult(num1, num2); // calling mult function with floating-point literal and variable.
+
+  return 0;
+}
+
+// Function definition
+void mult(float num1, float num2) {
+  float result = num1 * num2;
+  cout << "The result of multiplying " << num1 << " and " << num2<< " is: " << result <<endl;
+}
