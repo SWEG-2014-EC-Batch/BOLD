98a382f494a7c143046defd6cfc7540d7de220e8
HYT34 <124575355+HYT34@users.noreply.github.com>
2023-06-12 11:22:03 -0700

    Update and rename 5 to 5.cpp

---
+++ FoP-I/Homework_Function_Exercises/5.cpp
@@ -0,0 +1,17 @@
+//A program that adds two numbers by cubing  first number to the square root of second number 
+#include <iostream>
+#include <cmath>
+using namespace std;
+double calculate(double num1, double num2);
+int main() {
+    double num1, num2;
+    cout << "Enter the first number: ";
+    cin >> num1;
+    cout << "Enter the second number: ";
+    cin >> num2;
+    double result = calculate(num1, num2);
+    cout << "The result is: " << result << std::endl;
+    return 0;}
+double calculate(double num1, double num2) {
+double answer = pow(num1, 3) + sqrt(num2);
+return answer;}
