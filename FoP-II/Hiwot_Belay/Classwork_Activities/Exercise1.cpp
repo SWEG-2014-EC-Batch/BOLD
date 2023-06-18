056e6d19d5aa6ad89af0b9bf1dd2bc5bb63b94bf
Hiwot Belay Kebede <117976961+Hiwot2127@users.noreply.github.com>
2023-06-18 22:14:31 +0300

    Welcome_Msg

--- FoP-II/Hiwot_Belay/Classwork_Activities/Exercise1.cpp
+++ FoP-II/Hiwot_Belay/Classwork_Activities/Exercise1.cpp
@@ -1,1 +1,19 @@
+#include <iostream>
+#include <string>
+using namespace std;
+// to demonstrate function defination and declaration
 
+void welcomeMessage();
+
+int main() {
+    welcomeMessage();
+    return 0;
+}
+
+void welcomeMessage() {
+    string name;
+    cout << "Enter your name"<< endl;
+    cin >> name;
+    cout << "Hello " << name << "!"<< endl;
+    cout << "Welcome to M.P" << endl;
+}
