18b91a07f0b02b9fa0d606cd2b3e24a5dcecc159
fenitamas <124518729+fenitamas@users.noreply.github.com>
2023-06-19 21:58:10 +0300

    Add files via upload

---
+++ FoP-II/Kaku_Temesgen/Classwork_Activities/user_defined_header/calculator.h
@@ -0,0 +1,9 @@
+#ifndef CALCULATOR_H
+#define CALCULATOR_H
+
+#include<iostream>
+
+double getProduct(int num1, double num2) {
+    return num1 * num2;
+}
+#endif
---
+++ FoP-II/Kaku_Temesgen/Classwork_Activities/user_defined_header/product.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include "calculator.h"
+
+using namespace std;
+
+int main() {
+    int firstNum;
+    double secondNum;
+
+    cout << "Enter the first number (integer): ";
+    cin >> firstNum;
+    cout << "Enter the second number (double): ";
+    cin >> secondNum;
+
+    double product = getProduct(firstNum, secondNum);
+
+    cout << "The Product is: " << product << endl;
+
+    return 0;
+}
