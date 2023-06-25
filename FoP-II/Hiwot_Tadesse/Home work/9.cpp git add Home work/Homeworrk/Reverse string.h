701379e12db5d6a38a6d315ee5ad1e109fb0fd80
HYT34 <124575355+HYT34@users.noreply.github.com>
2023-06-25 21:54:14 +0300

    Update Reverse string.h

--- FoP-II/Hiwot_Tadesse/Home work/9.cpp git add Home work/Homeworrk/Reverse string.h
+++ FoP-II/Hiwot_Tadesse/Home work/9.cpp git add Home work/Homeworrk/Reverse string.h
@@ -1,1 +1,19 @@
+#ifndef REVERSE_STRING_H
+#define REVERSE_STRING_H
 
+using namespace std;
+
+string reverse_string(string& str) {
+    string reversed_string = ""; // create an empty string to store the reversed string
+    int i = 0;
+    while (str[i] != '\0') { // loop through the string until the null character is reached
+        i++;
+    }
+    for (int j = i - 1; j >= 0; j--) { // loop through the string backwards and add each character to the reversed string
+        reversed_string += str[j];
+    }
+    return reversed_string; // return the reversed string
+}
+
+
+#endif
