e619052d892ecd9c9bb441fb803145594dba448b
HYT34 <124575355+HYT34@users.noreply.github.com>
2023-06-25 21:45:53 +0300

    Update and rename 2 to Compare string.cpp

---
+++ FoP-II/Hiwot_Tadesse/Home work/9.cpp git add Home work/Homeworrk/Compare string.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include"compare strings.h"//include the header file user defined function 
+using namespace std;
+int main() {
+    string str1 ,str2;
+    cout <<"enter the first string";
+    cin>>str1;
+    cout <<"enter the second string ";
+    cin>>str2;
+     int result = stringCompare(str1, str2);
+    
+    if (result == 0) {
+        cout << "Both strings are equal";
+    }
+    else if (result == 1) {
+        cout << "String 1 is greater than string 2";
+    }
+    else {
+        cout << "String 2 is greater than string 1";
+    }
+    
+    return 0;
+}
