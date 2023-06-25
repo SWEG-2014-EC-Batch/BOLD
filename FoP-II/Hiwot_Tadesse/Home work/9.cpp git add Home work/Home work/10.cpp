ef602388b26297e4d119a049a847f67156a3263d
HYT34 <124575355+HYT34@users.noreply.github.com>
2023-06-25 22:03:58 +0300

    Update 10.cpp

--- FoP-II/Hiwot_Tadesse/Home work/9.cpp git add Home work/Home work/10.cpp
+++ FoP-II/Hiwot_Tadesse/Home work/9.cpp git add Home work/Home work/10.cpp
@@ -1,1 +1,77 @@
 
+//a program to convert feet and inch to meter and cm 
+//demonstration of types of parameter passing 
+#include<iostream>
+using namespace std;
+
+float input1(float *m,float *cm );
+float input2(float* feet,float* inch);
+float process1( float *feet, float* inch );
+float process2(float *m, float *cm);
+float output1();
+float output2();
+
+int main(){int choice;
+ do{//do while loop to repeat the program until quit is chose
+    cout<<"welocome to the converter program"<<endl;
+    cout<<"choose 1 .to convert from meter and cm to feet and inches"<<endl;
+    cout<<"choose 2.to convert from feet and inches to meter and cm"<<endl;
+    cout<<"choose 3.to quit the progarm";
+    cin>>choice;
+    switch (choice){//switch for the choices
+        case 1:
+        output1();//call function output1
+        break;
+        case 2:
+        output2();//call the function output2
+        break;
+        case 3:
+        cout<<"the progarm has ended";
+        return 0;
+        break;
+        default:
+        cout<<"no such choice";
+        } 
+        }while (choice<=3 );
+ }
+
+float input1(float *m,float *cm ){//the input function(enter values) to convert meter and cm to feet and inches with parameters passed by pointer
+
+    cout <<"enter the magnitude in meter";
+    cin >>*m;//to store the value entered on the adress given
+    cout <<"enter the magnitude in cm";
+    cin >>*cm;
+   }
+float input2(float* feet,float* inch){//the input(enter values) function to convert  feet and inches to meters and cm with parameters passed by pointer
+     
+    cout <<"enter the magnitude in feet";
+    cin >>*feet;
+    cout <<"enter the magnitude in inch";
+    cin >>*inch;
+    
+    }
+float process1( float *feet, float* inch ){//the calculater function have parameters passed by pointer
+    float m, cm;
+    input1(&m,&cm);//call the input1 function 
+    *feet=m*3.2;//calculate and put the result on the memory address of the parameter
+    *inch=cm*0.39;
+    
+    
+}
+
+float process2(float *m, float *cm){//the calculater function have parameters passed by pointer 
+    float feet,inch;
+    input2(&feet,&inch);//call the input2 function 
+   *m=feet*0.3125;//calculate and put the result on the memory address of the parameter
+   *cm=inch*2.54;
+}
+float output1(){float feet, inch;
+    process1(&feet,&inch);//call the function process1
+    
+cout<<"the magnitude in feet is "<<feet <<endl<<"the magnitude in inches is "<<inch<<endl;
+
+}
+float output2(){float m, cm;
+process2(&m,&cm);//call the function process2 
+cout<<"the magnitude in meters is "<<m<<endl<<"the magnitude in cm is "<<cm<<endl;
+}
