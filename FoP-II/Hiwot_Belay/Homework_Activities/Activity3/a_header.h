d705caa1fed4aa05385b5b27b7b90282682cda22
Hiwot Belay Kebede <117976961+Hiwot2127@users.noreply.github.com>
2023-06-26 23:27:52 +0300

    header for a

---
+++ FoP-II/Hiwot_Belay/Homework_Activities/Activity3/a_header.h
@@ -0,0 +1,9 @@
+#ifndef HEADER_FILE_H
+#define HEADER_FILE_H
+
+//Function declarations for a
+int stringLength(const char* str);
+int countVowels(const char* str);
+int countConsonants(const char* str);
+
+#endif
