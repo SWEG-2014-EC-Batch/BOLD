530a9f823c1d5bc4e51f6137412a0dc204910608
Hiwot Belay Kebede <117976961+Hiwot2127@users.noreply.github.com>
2023-07-11 20:50:15 +0300

    Phone Record

---
+++ FoP-II/Hiwot_Belay/Homework_Activities/Activity4/Ex3.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+// Structure to store the phone records of a person
+struct PhoneRecord {
+  string name;
+  int areacode;
+  int exchanger;
+  int no;
+};
+
+void getPhoneRecords(PhoneRecord records[]);
+void printPhoneRecords(PhoneRecord records[]);
+
+int main() {
+
+  PhoneRecord records[10];
+  getPhoneRecords(records);
+  printPhoneRecords(records);
+
+  return 0;
+}
+
+void getPhoneRecords(PhoneRecord records[]) {
+  for (int i = 0; i < 10; i++) {
+    cout << "Enter the name of person " << i + 1 << ": ";
+    cin >> records[i].name;
+    cout << "Enter the area code of person " << i + 1 << ": ";
+    cin >> records[i].areacode;
+    cout << "Enter the exchanger of person " << i + 1 << ": ";
+    cin >> records[i].exchanger;
+    cout << "Enter the phone number of person " << i + 1 << ": ";
+    cin >> records[i].no;
+  }
+}
+
+void printPhoneRecords(PhoneRecord records[]) {
+    cout<<"Name\t\t\t Phone number\n";
+  for (int i = 0; i < 10; i++) {
+    cout<< records[i].name <<"\t\t\\t "<< records[i].areacode <<"-"<< records[i].exchanger <<"-"<< records[i].no << endl;
+  }
+}
