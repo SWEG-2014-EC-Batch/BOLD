9fa826aa4f6278195bb78dc61d6e283c4c719a10
HYT34 <124575355+HYT34@users.noreply.github.com>
2023-07-12 06:51:06 -0700

    Update and rename ex1 to No 1.cpp

---
+++ FoP-II/Hiwot_Tadesse/File management/No 1.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+void readFile(const char* fileName, int array[], int& count);
+
+int main() {
+    const char* fileName = "input.txt"; //this should be the real path of the file on your respective computers
+    const int SIZE = 100;
+    int array[SIZE];
+    int count = 0;
+
+    readFile(fileName, array, count);
+
+    cout << "Read " << count << " entries from the file." << endl;
+    cout << "Contents of the array: ";
+    for (int i = 0; i < count; i++) {
+        cout << array[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
+
+void readFile(const char* fileName, int array[], int& count) {
+    ifstream inputFile(fileName);
+
+    if (inputFile.is_open()) {
+        count = 0;
+
+        while (!inputFile.eof()) {
+            inputFile >> array[count];
+            count++;
+        }
+        inputFile.close();
+    } else {
+        cout << "Error opening file: " << fileName << endl;
+    }
+}
