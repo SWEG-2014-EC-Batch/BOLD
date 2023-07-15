6ed55058e97d5fcc83a1f24a2213cf4db613e8d0
HYT34 <124575355+HYT34@users.noreply.github.com>
2023-07-15 06:04:48 -0700

    Update P1_Pattern_Version1_Refactored.cpp

--- FoP-II/Project_Work_Refactored/P1_Pattern_Version1_Refactored.cpp
+++ FoP-II/Project_Work_Refactored/P1_Pattern_Version1_Refactored.cpp
@@ -13,3 +13,18 @@ int main()
     {
         int n;
         getInput(n); // Get input from user
+cout << endl;
+        printUpperHalf(n); // Print upper half of pattern
+        printLowerHalf(n); // Print lower half of pattern
+
+        cout << "\n";
+        cout << "Do you want to print again? (Y/N): ";
+        cin >> choice;
+    } while (choice == 'Y' || choice == 'y');
+
+    return 0;
+}
+
+// Function to get input from user
+void getInput(int &n)
+{
