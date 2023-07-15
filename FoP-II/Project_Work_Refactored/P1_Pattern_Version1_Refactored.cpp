3360ff9e889e5c057b9967bc505f99aae11fb4a5
Hiwot Belay Kebede <117976961+Hiwot2127@users.noreply.github.com>
2023-07-15 13:02:22 +0300

    Update P1_Pattern_Version1_Refactored.cpp

--- FoP-II/Project_Work_Refactored/P1_Pattern_Version1_Refactored.cpp
+++ FoP-II/Project_Work_Refactored/P1_Pattern_Version1_Refactored.cpp
@@ -1,1 +1,15 @@
+#include <iostream>
+using namespace std;
 
+// A program that prints a character Hourglass pattern
+void getInput(int &n);
+void printUpperHalf(int n);
+void printLowerHalf(int n);
+
+int main()
+{
+    char choice;
+    do
+    {
+        int n;
+        getInput(n); // Get input from user
