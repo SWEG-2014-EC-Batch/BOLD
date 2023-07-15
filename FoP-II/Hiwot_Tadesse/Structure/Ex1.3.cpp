94eb4929b12f900891f8bf96576eedf3255f0a3a
HYT34 <124575355+HYT34@users.noreply.github.com>
2023-07-15 04:53:50 -0700

    Create Ex1.3.cpp

---
+++ FoP-II/Hiwot_Tadesse/Structure/Ex1.3.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+struct point {
+    float x;
+    float y;
+};
+
+point read() {
+    point p;
+    cout << "Enter x and y coordinates: ";
+    cin >> p.x >> p.y;
+    return p;
+}
+
+void print(point p) {
+    cout << "The coordinates are: (" << p.x << ", " << p.y << ")" << endl;
+}
+
+int main() {
+    point p = read();
+    print(p);
+    return 0;
+}
