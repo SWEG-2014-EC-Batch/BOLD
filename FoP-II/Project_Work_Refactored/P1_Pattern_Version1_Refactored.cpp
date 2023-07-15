9e28bf4c770f0ab266f0aed98a6ff118d5ae1963
fenitamas <124518729+fenitamas@users.noreply.github.com>
2023-07-15 20:10:34 +0300

    Update P1_Pattern_Version1_Refactored.cpp

--- FoP-II/Project_Work_Refactored/P1_Pattern_Version1_Refactored.cpp
+++ FoP-II/Project_Work_Refactored/P1_Pattern_Version1_Refactored.cpp
@@ -43,3 +43,18 @@ void getInput(int &n)
 // Function to print upper half of pattern
 void printUpperHalf(int n)
 {
+for (int i = n; i >= 1; i--)
+    { // Iterate from n to 1
+        for (int k = n; k >= i; k--)
+        {                 // Iterate from n to i
+            cout << "  "; // Print 2 spaces
+        }
+        for (int j = 1; j <= (2 * i) - 1; j++)
+        { // Iterate from 1 to (2*i)-1
+            if (j <= i)
+            {
+                cout << (char)('A' + j - 1) << " "; // Print letter
+            }
+            else
+            {
+                cout << (char)('A' + (2 * i) - j - 1) << " "; // Print letter
