58d78e3dfb57d43d4b51bf95fceab5e9da37c791
Hiwot Belay Kebede <117976961+Hiwot2127@users.noreply.github.com>
2023-07-15 13:03:31 +0300

    Update Project-1_Refactored.cpp

--- FoP-II/Project_Work_Refactored/Project-1_Refactored.cpp
+++ FoP-II/Project_Work_Refactored/Project-1_Refactored.cpp
@@ -1,1 +1,20 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
 
+// An encryption/decryption program that allows the company to securely transmit four-digit data over the telephone
+void encrypt(int *);
+void decrypt(int *);
+void displayResult(const int *);
+void acceptNumber(int &, char &);
+
+int main()
+{
+    int number;
+    char choice;
+
+    cout << "\n\nWelcome to the Data Encryption/Decryption Program!" << endl;
+    cout << "This program allows you to securely transmit a four-digit integer over the telephone." << endl;
+
+    do
+    {
