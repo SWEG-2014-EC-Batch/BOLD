d679adb16f15bc81f97f189c33d39c378c727050
Hiwot Belay Kebede <117976961+Hiwot2127@users.noreply.github.com>
2023-09-30 19:41:32 +0300

    Logo file

---
+++ FoP-II/Final_Project/logo.h
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+
+//Logo file for the Employee/payroll Management System
+int logo()
+{
+  cout << "\n";
+
+  cout << "                            ____                       _ _                 _                 \n";
+  cout << "                           |  _ \\ __ _ _   _ _ __ ___ | | |  ___ _   _ ___| |_ ___ _ __ ___  \n";
+  cout << "                           | |_) / _` | | | | '__/ _ \\| | | / __| | | / __| __/ _ \\ '_ ` _ \\ \n";
+  cout << "                           |  __/ (_| | |_| | | | (_) | | | \\__ \\ |_| \\__ \\ ||  __/ | | | | |\n";
+  cout << "                           |_|   \\__,_|\\__, |_|  \\___/|_|_| |___/\\__, |___/\\__\\___|_| |_| |_|\n";
+  cout << "                                       |___/                     |___/                        \n";
+  return 0;
+}
+
+int splash()
+{
+  cout << "\t\t\t                                               ,,    ,,    \n"
+          "\t\t\t                                             `7MM  `7MM    \n"
+          "\t\t\t                                               MM    MM    \n"
+          "\t\t\t`7MMpdMAo.  ,6\"Yb.`7M'   `MF'`7Mb,od8 ,pW\"Wq.  MM    MM    \n"
+          "\t\t\t  MM   `Wb 8)   MM  VA   ,V    MM' \"'6W'   `Wb MM    MM    \n"
+          "\t\t\t  MM    M8  ,pm9MM   VA ,V     MM    8M     M8 MM    MM    \n"
+          "\t\t\t  MM   ,AP 8M   MM    VVV      MM    YA.   ,A9 MM    MM    \n"
+          "\t\t\t  MMbmmd'  `Moo9^Yo.  ,V     .JMML.   `Ybmd9'.JMML..JMML.  \n"
+          "\t\t\t  MM                 ,V                                    \n"
+          "\t\t\t.JMML.            OOb\"                                     \n"
+          "\t\t\t                                                            \n"
+          "\t\t\t                                                            \n"
+          "\t\t\t                             mm                            \n"
+          "\t\t\t                             MM                            \n"
+          "\t\t\t,pP\"Ybd `7M'   `MF',pP\"Ybd mmMMmm .gP\"Ya `7MMpMMMb.pMMMb.  \n"
+          "\t\t\t8I   `\"   VA   ,V  8I   `\"   MM  ,M'   Yb  MM    MM    MM  \n"
+          "\t\t\t`YMMMa.    VA ,V   `YMMMa.   MM  8M\"\"\"\"\"\"  MM    MM    MM  \n"
+          "\t\t\tL.   I8     VVV    L.   I8   MM  YM.    ,  MM    MM    MM  \n"
+          "\t\t\tM9mmmP'     ,V     M9mmmP'   `Mbmo`Mbmmd'.JMML  JMML  JMML.\n"
+          "\t\t\t           ,V                                               \n"
+          "\t\t\t        OOb\"                                                \n";
+
+  cout << "\t\t\t.............Press any Key to Enter the System ";
+  char opt;
+  cin >> opt;
+  return 0;
+}
